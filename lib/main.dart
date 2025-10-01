132ae7ab483a18495a3f6a3d8b541a150c5a57cb
ClaretWheel1481 <LanceHuang245@outlook.com>
2025-10-01 23:54:08 +0800

    chore: dependencies update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:zephyr/app.dart';
 import 'core/startup.dart';
 
+// TODO: 极光推送、fcm接入
 Future<void> main() async {
   await initAppSettings();
 
