506cf2cd1075b16be8257f1f7798a29e31a92a6c
ClaretWheel1481 <LanceHuang245@outlook.com>
2025-10-23 23:58:41 +0800

    fix: 修复iOS后台自动获取数据

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,29 +1,25 @@
-import Flutter
 import UIKit
+import Flutter
 import workmanager_apple
 
-func registerPlugins(registry: FlutterPluginRegistry) {
-  GeneratedPluginRegistrant.register(with: registry)
-}
-
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
 
-    // Enable background fetch.
-    UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
+        WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+            GeneratedPluginRegistrant.register(with: registry)
+        }
 
-    // Register the background task with the scheduler.
-    WorkmanagerPlugin.registerBGTaskScheduler(withIdentifier: "org.claret.easyweather.fetchWeatherTask")
+        WorkmanagerPlugin.registerPeriodicTask(
+            withIdentifier: "org.claret.easyweather.fetchWeatherTask",
+            frequency: NSNumber(value: 30 * 60) // 30 分钟
+        )
 
-    // Set the plugin registrant callback to register plugins in the background isolate.
-    WorkmanagerPlugin.setPluginRegistrantCallback(registerPlugins)
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
 
